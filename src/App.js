8ad7434a62b8a2949cb071f417e5f9f287b42113
mayashley <54293844+mayashley@users.noreply.github.com>
2019-12-18 13:33:11 -0500

    12-18-19
    today I made the navbar responsive, added target blank to open social media in a new tab and spell checked the about me page. (need to double check the wntire site one more time and add all of my new projects to it before deployment.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navigation/Navbar";
+// import SideNav from "./components/navigation/SideNav";
 import "./App.scss";
 import ContactForm from "./components/contactPage/ContactForm";
 import Projects from "./components/ProjectsPage/Projects";
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <div className="bodyContainer">
         <Navbar />
+        {/* <SideNav/> */}
         <Switch>
           <Route path="/contact" component={ContactForm} />
           <Route path="/about" component={AboutMe} />
--- src/components/AboutPage/SocialLinks.js
+++ src/components/AboutPage/SocialLinks.js
@@ -7,15 +7,15 @@ class SocialLinks extends Component {
       <div className="socialLinks">
         <div className="containLinks">
           <div className="links">
-            <a href="https://www.linkedin.com/in/ashley-may-a145a4164/">
+            <a href="https://www.linkedin.com/in/ashley-may-a145a4164/" target="_blank"  rel="noopener noreferrer">
             <i className="fab fa-linkedin-in"></i>
             </a>
           </div>
           <div className="links">
-            <a href="https://github.com/mayashley"><i className="fab fa-github"></i></a>
+            <a href="https://github.com/mayashley" target="_blank"  rel="noopener noreferrer"><i className="fab fa-github"></i></a>
           </div>
           <div className="links">
-            <a href="https://www.freecodecamp.org/fcc214a97eb-4900-4876-9bed-3c4c00af75ba">
+            <a href="https://www.freecodecamp.org/fcc214a97eb-4900-4876-9bed-3c4c00af75ba" target="_blank"  rel="noopener noreferrer">
             <i className="fab fa-free-code-camp"></i>
             </a>
           </div>
